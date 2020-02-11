4efcbcdbc46a09bb65510565ae1c139f68ee804a
Hem Sunder <hemsunder47@gmail.com>
2020-02-11 15:22:57 +0530

    login with google done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService, GoogleLoginProvider } from 'angular4-social-login';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'loginwithgoogle';
+  title = 'login with google';
+
+  user: any;
+
+  constructor(private _socioAuthServ: AuthService) {  }
+
+  singIn(platform : string): void {
+    platform = GoogleLoginProvider.PROVIDER_ID;
+    this._socioAuthServ.signIn(platform).then(
+      (response) => {
+        console.log(platform + " logged in user data is= " , response);
+        this.user = response;
+      }
+    );
+  }
+ 
+  // Method to log out.
+  signOut(): void {
+    this._socioAuthServ.signOut();
+    this.user = null;
+    console.log('User signed out.');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider } from 'angular4-social-login';
+
+const google_oauth_client_id:string = '750040336077-gsga2orrke7846q2394h2h65bp2qdd4o.apps.googleusercontent.com';
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider(google_oauth_client_id)
+  }
+]);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocialLoginModule.initialize(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
